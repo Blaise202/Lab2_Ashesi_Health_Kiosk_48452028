c3df307f79565a0c7d168b3dd2d62473e0c1dddb
Blaise202 <iblaise2023@gmail.com>
2025-09-27 01:25:40 +0000

    Task 1

--- HealthKiosk.java
+++ HealthKiosk.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
+
 public class HealthKiosk{
   public static void main(String[] args){
-    Scanner scanner = Scanner(Syestem.in);
-    System.out.println("Welcome to our program!");
-    String letter = scanner.nextLine();
-    switch(letter){
-      case 'A':
-        System.out.println("Excellent");
+    // Task 1
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please Select the service code: P/L/T/C: ");
+    char service_code = scanner.next().charAt(0);
+    switch(service_code.toUpperCase()){
+      case 'P':
+        System.out.println("Go To: Pharmacy Desk");
         break;
-      case 'B':
-        System.out.println("Pass");
+      case 'L':
+        System.out.println("Go To: Lab Desk");
+        break;
+      case 'T':
+        System.out.println("Go To: Triage Desk");
+        break;
+      case 'C':
+        System.out.println("Go To: Counseling Desk");
         break;
       default:
-        System.out.println("fail");
-        break;
+        System.out.println("Invalid Service Code");
     }
   }
 }
